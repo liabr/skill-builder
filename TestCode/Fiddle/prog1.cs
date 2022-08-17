a4f46cc5d35f79408aeec44024bbb48e8ad93a0f
liabr <lijo.abraham@bjss.com>
2022-08-17 12:31:48 -0500

    tst

---
+++ TestCode/Fiddle/prog1.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestCode.Fiddle{
+    class prog1{
+        public static void main(String[] args){
+            Console.WriteLine("fdds");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ReadKey();
+        }
+    }
+}
